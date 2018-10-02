c28147490b259b30bb7a5663b6c1cce7a6351647
Gareth Bk <garethbk@gmail.com>
2018-10-02 16:45:02 -0700

    Added first mutation for ingredient and resolver to save new ingredient to Mongo

--- src/index.js
+++ src/index.js
@@ -8,10 +8,40 @@ mongoose.connect(MONGO_URI);
 
 const resolvers = {
   Query: {
+    ingredients: async () => {
+      const ingredients = await Ingredient.find().lean();
+      //console.log(ingredients);
+
+      const allIngredients = ingredients.map(ingredient => {
+        const { name, calories, dietary } = ingredient;
+        return {
+          name,
+          calories,
+          dietary,
+        };
+      });
+
+      console.log(allIngredients);
+
+      return { ingredients: allIngredients };
+    },
+
     ingredient: async (_, { _id }) => {
-      return await Ingredient.findById({ _id }).lean();
+      const ingredient = await Ingredient.findById({ _id }).lean();
+
+      console.log(ingredient);
 
       return ingredient;
+    },
+  },
+
+  Mutation: {
+    createIngredient: async (_, args) => {
+      const newIngredient = new Ingredient(args);
+
+      newIngredient.save();
+
+      return newIngredient;
     },
   },
 };
